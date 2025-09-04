555d1f4ae5fdbb0f9cc8587f3e3fcd35ade6cb4a
Praveen Kumar <ipraveenkumar.v@gmail.com>
2025-09-04 12:53:37 +0530

    Update Caesar Cipher and function files

---
+++ function.py
@@ -0,0 +1,8 @@
+# def greet(name):
+#     print("hi", name)
+#     print("how are you", name)
+#     print(name, "name is unique")
+# greet("Inthiyaz")
+def greet(name, loc):
+    print(f"Hi {name}, your are from {loc}")
+greet("Inthiyaz", "Switzerland")
